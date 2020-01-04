032956cb26c0ce23ef17b8e956bdb3539e0e5cd2
Eugene Ahn <01.ahn.eugene@gmail.com>
2020-01-04 11:29:39 -0800

    Adding About and Contact us page

---
+++ components/ContactComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Text,ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    };
+
+    render(){ 
+        return(
+            //loads all its child components 
+            <ScrollView>
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}} >
+                    <Text   style ={{ marginBottom:10 }} >
+                            1 Nucamp Way {"\n"}
+                            Seattle, WA 98001 {"\n"}
+                            U.S.A.
+                    </Text>
+                    <Text>
+                            Phone : 1-206-555-1234
+                    </Text>
+                    <Text   style ={{ marginBottom:10 }} >
+                            Email : campsites@nucamp.co
+                    </Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
+}
+export default Contact; 
