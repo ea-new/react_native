d8fcd92131ee8059c988a8f75c719fcc3af2f147
Eugene Ahn <01.ahn.eugene@gmail.com>
2020-01-10 23:18:38 -0800

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -29,7 +30,7 @@ class About extends Component {
     };
 
     render(){ 
-        const renderPartner = ({item}) => { 
+        const renderPartner = ({item}) => {
             return (
                 <ListItem
                     title={item.name}
@@ -37,11 +38,31 @@ class About extends Component {
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
+        };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
         }
-
-        return(
-            //loads all its child components 
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        return (
             <ScrollView>
                 <Mission/>
                 <Card title = "Community Partners" >
