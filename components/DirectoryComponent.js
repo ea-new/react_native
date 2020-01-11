c8a445d20139e4c28aac9152b2ef5a24a58297ea
Eugene Ahn <01.ahn.eugene@gmail.com>
2020-01-10 22:49:27 -0800

    Using Redux

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { CAMPSITES} from '../shared/campsites'; 
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites
+    };
+};
 
 
 class Directory extends Component {
-    
-    constructor(props){
-        super(props); 
-        this.state = { 
-            campsites: CAMPSITES
-        }; 
-    }
+
 
     static navigationOptions = {
         title: 'Directory'
@@ -22,23 +23,24 @@ class Directory extends Component {
 
         const renderDirectoryItem = ({item}) => {
             return (
-                <ListItem
+                <Tile
                     title={item.name}
-                    subtitle={item.description}
-                    onPress={() => navigate('CampsiteInfo', {campsiteId:item.id })}
-                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                    caption={item.description}
+                    featured
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    imageSrc={{uri: baseUrl + item.image}}
                 />
             );
         };
 
         return (
-            <FlatList 
-                data={this.state.campsites}
+            <FlatList
+                data={this.props.campsites.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
             />
         );
-    }   
+    }
 }
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
